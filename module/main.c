3ed2ecf4aa185ef47ff89453a80ee1c082089cc0
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2023-11-26 13:06:08 +0100

    moved .vscode

--- module/main.c
+++ module/main.c
@@ -5,6 +5,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include "generated/hello_world.h"
 
 MODULE_AUTHOR("Davide Collovigh");
 MODULE_DESCRIPTION("pbtools_lkm_main: protobuf LKM");
@@ -13,7 +14,46 @@ MODULE_VERSION("0.1");
 
 static int __init pbtools_lkm_init(void)
 {
-	pr_info("Hello, world\n");
+
+    pr_info("Start");
+
+    int size;
+    uint8_t workspace[64];
+    uint8_t encoded[16];
+    struct hello_world_foo_t *foo_p;
+
+    /* Encode. */
+    foo_p = hello_world_foo_new(&workspace[0], sizeof(workspace));
+
+    if (foo_p == NULL) {
+        return 1;
+    }
+
+    foo_p->bar = 78;
+    size = hello_world_foo_encode(foo_p, &encoded[0], sizeof(encoded));
+
+    if (size < 0) {
+        return 2;
+    }
+
+    pr_info("Successfully encoded Foo into %d bytes.\n", size);
+
+    /* Decode. */
+    foo_p = hello_world_foo_new(&workspace[0], sizeof(workspace));
+
+    if (foo_p == NULL) {
+        return 3;
+    }
+
+    size = hello_world_foo_decode(foo_p, &encoded[0], size);
+
+    if (size < 0) {
+        return 4;
+    }
+
+    pr_info("Successfully decoded %d bytes into Foo.\n", size);
+    pr_info("Foo.bar: %d\n", foo_p->bar);
+
 	return 0;		/* success */
 }
 
