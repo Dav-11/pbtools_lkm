230aaa9fc44d053e57b36009a8de155f7b970f02
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2024-04-07 08:39:45 +0000

    makefile tab instead of spaces

--- pbtools_lkm/lkm_common/generate_makefile.py
+++ pbtools_lkm/lkm_common/generate_makefile.py
@@ -6,60 +6,60 @@ MAKEFILE_FMT = '''
 # on a Raspberry Pi implies that it's the host)
 KDIR ?= /lib/modules/$(shell uname -r)/build
 
-PWD               := $(shell pwd)
-EXTRA_CFLAGS       += -DDEBUG
-obj-m             += {module_name}.o
+PWD			   := $(shell pwd)
+EXTRA_CFLAGS	   += -DDEBUG
+obj-m			 += {module_name}.o
 
-{module_name}-y     := \\
-    main.o \\
-    pbtools_lkm.o \\
-    generated/{module_name}.o
+{module_name}-y	 := \\
+	main.o \\
+	pbtools_lkm.o \\
+	generated/{module_name}.o
 
 all: build install load
 
 build:
-    @echo
-    @echo '--- Building : KDIR=${{KDIR}} ARCH=${{ARCH}} CROSS_COMPILE=${{CROSS_COMPILE}} EXTRA_CFLAGS=${{EXTRA_CFLAGS}} ---'
-    @echo
+	@echo
+	@echo '--- Building : KDIR=${{KDIR}} ARCH=${{ARCH}} CROSS_COMPILE=${{CROSS_COMPILE}} EXTRA_CFLAGS=${{EXTRA_CFLAGS}} ---'
+	@echo
 
-    # build
-    $(MAKE) -C $(KDIR) M=$(PWD) modules
+	# build
+	$(MAKE) -C $(KDIR) M=$(PWD) modules
 
 install:
-    @echo
-    @echo "--- Installing module {module_name} ---"
-    @echo
+	@echo
+	@echo "--- Installing module {module_name} ---"
+	@echo
 
-    sudo make -C $(KDIR) M=$(PWD) modules_install
-    sudo depmod
+	sudo make -C $(KDIR) M=$(PWD) modules_install
+	sudo depmod
 
 load:
-    @echo
-    @echo "--- Loading module into the kernel ---"
-    @echo
-    sudo insmod $(PWD)/{module_name}.ko
+	@echo
+	@echo "--- Loading module into the kernel ---"
+	@echo
+	sudo insmod $(PWD)/{module_name}.ko
 
 unload:
-    @echo
-    @echo "--- Removing the module from the kernel ---"
-    @echo
-    sudo rmmod {module_name}
+	@echo
+	@echo "--- Removing the module from the kernel ---"
+	@echo
+	sudo rmmod {module_name}
 
 clean:
-    @echo
-    @echo "--- Cleaning ---"
-    @echo
-    $(MAKE) -C $(KDIR) M=$(PWD) clean
+	@echo
+	@echo "--- Cleaning ---"
+	@echo
+	$(MAKE) -C $(KDIR) M=$(PWD) clean
 
 help:
-    @echo targets:
-    @echo       build: compile the LKM
-    @echo       install: install the LKM
-    @echo       load: load the LKM into the running Linux OS
-    @echo       unload: remove the LKM from the Linux OS
-    @echo
-    @echo       help: show this message
-    @echo       clean: clear all the files created by the compile process
+	@echo targets:
+	@echo	   build: compile the LKM
+	@echo	   install: install the LKM
+	@echo	   load: load the LKM into the running Linux OS
+	@echo	   unload: remove the LKM from the Linux OS
+	@echo
+	@echo	   help: show this message
+	@echo	   clean: clear all the files created by the compile process
 '''
 
 
