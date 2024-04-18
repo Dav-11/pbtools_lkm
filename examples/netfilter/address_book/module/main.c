bd122924475f1cb7b2814d1e8ebed6e08f086f98
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2024-04-18 16:58:17 +0000

    netfilter/address_book OK

--- examples/netfilter/address_book/module/main.c
+++ examples/netfilter/address_book/module/main.c
@@ -14,23 +14,18 @@
 
 #include "../../../common/address_book/generated/address_book.h"
 
-MODULE_AUTHOR("Davide Collovigh");
-MODULE_DESCRIPTION("netfilter_example: address_book protobuf");
+MODULE_AUTHOR("Your Name Here");
+MODULE_DESCRIPTION("address_book_nf: some description");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
 
-#define MYPORT          60001
-#define MAX_PAYLOAD_LEN 1024 // Maximum length of payload to print
+#define MY_PORT          60001
+#define BUFFER_SIZE 1024
 
+//struct holding set of hook function options
+static struct nf_hook_ops nfho;
 
-static struct nf_hook_ops nfho;         //struct holding set of hook function options
-
-typedef struct {
-    uint32_t size;
-    unsigned char encoded[MAX_PAYLOAD_LEN];
-} message;
-
-// Function to print TCP payload
+// Function to print payload
 void print_hex(const unsigned char *payload, unsigned int payload_len)
 {
     unsigned int i;
@@ -40,22 +35,22 @@ void print_hex(const unsigned char *payload, unsigned int payload_len)
     pr_info("\n");
 }
 
-unsigned int process_address_book(message *data)
+unsigned int process_message(char *buffer, int size)
 {
+
     uint8_t workspace[1024];
 
     struct address_book_address_book_t *address_book_p;
     struct address_book_person_t *person_p;
     struct address_book_phone_number_t *phone_number_p;
 
-    /* 
+    /*
      * Decode the message.
      */
     address_book_p = address_book_address_book_new(&workspace[0], sizeof(workspace));
 
-    address_book_address_book_decode(address_book_p, &data->encoded[0], data->size);
+    address_book_address_book_decode(address_book_p, buffer, size);
     pr_info("people.length: %d", address_book_p->people.length);
-
 
     /*
      * checks:
@@ -99,55 +94,58 @@ unsigned int process_address_book(message *data)
     return NF_ACCEPT;
 }
 
+/**
+ * This function reads the first 4 bytes of the payload extracting the size of the message,
+ * if the size is bigger than the buffer size, returns -1
+ */
+static int extract_message_size(char *buffer)
+{
+    // extract first 4 bytes (int) as protobuf size
+    int size = (uint32_t) ntohl(*((uint32_t *) buffer));
+
+    if (size < BUFFER_SIZE) {
+
+        pr_info("Found size: %u\n", size);
+        return size;
+    } else {
+
+        pr_err("Payload is too big for the specified buffer: [wanted: %u, got: %d]\n", size, BUFFER_SIZE);
+        return -1;
+    }
+}
+
 unsigned int handle_tcp_payload(struct sk_buff *skb)
 {
     struct iphdr *ip;
     struct tcphdr *tcp;
 
-    unsigned char *payload;
-    char str[MAX_PAYLOAD_LEN];
-    memset(str, 0, MAX_PAYLOAD_LEN);
+    unsigned char *buffer;
 
     ip = ip_hdr(skb); // Update IP header pointer
     tcp = tcp_hdr(skb); // Update TCP header pointer
 
-    uint32_t payload_len    = ntohs(ip->tot_len) - (ip->ihl * 4) - (tcp->doff * 4);
-
+    uint32_t payload_len = ntohs(ip->tot_len) - (ip->ihl * 4) - (tcp->doff * 4);
     pr_info("Payload length: %d\n", payload_len);
 
     if (payload_len > 0) {
 
-        payload = (unsigned char *)(tcp) + (tcp->doff * 4);
-        payload_len = min(payload_len, MAX_PAYLOAD_LEN); // Limit payload length to avoid excessive printing
+        buffer = (unsigned char *)(tcp) + (tcp->doff * 4);
 
-        // print full payload in hex
-        //pr_info("Payload:\n");
-        //print_hex(payload, payload_len);
+        // extract size of protobuf from buffer
+        int size = extract_message_size(buffer);
 
-        // create message struct
-        message data;
-        memset(&data, 0, sizeof(data));
+        if (size > 0) {
 
-        // copy in data.encoded memory from payload excluding first 4 bytes (len)
-        memcpy(data.encoded, payload + 4, payload_len - 4);
-
-        // get size (first 4 bytes)
-        data.size = (uint32_t) ntohl(*((uint32_t *) payload));
-        pr_info("Found size: %u\n", data.size);
-
-        //pr_info("Data.encoded:\n");
-        //print_hex(data.encoded, strlen(data.encoded));
-
-        return process_address_book(&data);
+            // Process received data as needed
+            return process_message(buffer + 4, size);
+        }
     }
 
     return NF_ACCEPT;
 }
 
-//function to be called by hook
 unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
-
     struct iphdr *ip;
     struct tcphdr *tcp;
 
@@ -158,7 +156,7 @@ unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_sta
 
     ip = ip_hdr(skb);
 
-    // consider only tcp (grpc is tcp)
+    // consider only tcp
     if (ip->protocol == IPPROTO_TCP && ip->daddr == htonl(INADDR_LOOPBACK)) {
 
         // get header tcp
@@ -168,26 +166,26 @@ unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_sta
         uint16_t src_port = ntohs(tcp->source);
         uint16_t dest_port = ntohs(tcp->dest);
 
-        if (dest_port == MYPORT && tcp->psh) {
+        if (dest_port == MY_PORT && tcp->psh) {
 
             pr_info("Received TCP packet. Source Port:%d, Destination Port:%d PUSH: %d\n", src_port, dest_port, tcp->psh);
-            
+
             // check if linear
             non_linear = skb_is_nonlinear(skb);
             if (non_linear) {
 
                 //pr_info("is_nonlinear: %d", non_linear);
-                
+
                 skb_linear = skb_copy(skb, GFP_ATOMIC); // Make a copy to preserve the original skb
                 if (!skb_linear) {
                     printk(KERN_ERR "Failed to allocate linearized skb\n");
-                    return NF_DROP; // Drop packet if linearization fails
+                    return NF_DROP; // Drop packet if copy fails
                 }
 
                 // linearize
                 int ret = skb_linearize(skb_linear);
                 if (ret != 0) {
-                    
+
                     printk(KERN_ERR "Failed to linearize skb\n");
                     kfree_skb(skb_linear); // Free the linearized skb
                     return NF_DROP; // Drop packet if linearization fails
@@ -201,12 +199,12 @@ unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_sta
                 return handle_tcp_payload(skb);
             }
         }
-	}
+    }
 
     return NF_ACCEPT;
 }
 
-static int __init pbtools_lkm_init(void)
+static int __init address_book_nf_init(void)
 {
     pr_info("Loaded module\n");
 
@@ -214,19 +212,18 @@ static int __init pbtools_lkm_init(void)
     nfho.hooknum    = NF_INET_LOCAL_IN;         /* packets to this machine */
     nfho.pf         = PF_INET;                  /* IPv4 */
     nfho.priority   = NF_IP_PRI_FIRST;          /* min hook priority */
-    
-    nf_register_net_hook(&init_net, &nfho);
 
+    nf_register_net_hook(&init_net, &nfho);
     pr_info("Registered nethook function\n");
 
     return 0;
 }
 
-static void __exit pbtools_lkm_exit(void)
+static void __exit address_book_nf_exit(void)
 {
     nf_unregister_net_hook(&init_net, &nfho);
     pr_info("Removed module\n");
 }
 
-module_init(pbtools_lkm_init);
-module_exit(pbtools_lkm_exit);
+module_init(address_book_nf_init);
+module_exit(address_book_nf_exit);
